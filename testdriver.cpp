8efe32590849f2586a7eb2ddac193ad530f98db7
John Wot <33kingkiller@users.noreply.github.com>
2017-01-07 14:18:59 -0600

    testdriver.cpp Update 2
    *Updated testdriver.cpp to better display more of the library's functions

--- testdriver.cpp
+++ testdriver.cpp
@@ -10,5 +10,29 @@ int main() {
 	jsonStream.input(objects, arrays);
 	jsonStream.close();
 
+	JsonMember tempMembersB[50]; //Boolean members
+	JsonMember tempMembersI[50]; //Integer members
+	JsonMember tempMembersS[50]; //String members
+
+	//Print first bool in the first object of the first array:
+	arrays[0].getJsonObject(0).getBoolMembers(tempMembersB);
+	if (tempMembersB[0].getBoolValue()) {
+		std::cout << tempMembersB[0].getName() << std::endl << "true\n";
+	}
+	else if (!tempMembersB[0].getBoolValue()) {
+		std::cout << tempMembersB[0].getName() << std::endl << "false\n";
+	}
+	else {
+		std::cout << "Failed to find boolean member.\n";
+	}
+
+	//Print first int in the first object of the first array:
+	arrays[0].getJsonObject(0).getIntMembers(tempMembersI);
+	std::cout << tempMembersI[0].getName() << std::endl << tempMembersI[0].getIntValue() << std::endl;
+
+	//Print first string in the first object of the first array:
+	arrays[0].getJsonObject(0).getStringMembers(tempMembersS);
+	std::cout << tempMembersS[0].getName() << std::endl << tempMembersS[0].getStringValue() << std::endl;
+
 	return 0;
 }
